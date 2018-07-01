122688e5984ef5b9fb8738ff6249664312f888d0
Nazanin Sabri <nazanin.sabrii@gmail.com>
2018-07-01 07:35:53 +0430

    add the correct files

--- initial/src/main/java/hello/Application.java
+++ initial/src/main/java/hello/Application.java
@@ -1,34 +1,15 @@
 package hello;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
 
 @SpringBootApplication
-public class Application {
+@EnableScheduling
+public class Application{
 
-	private static final Logger log = LoggerFactory.getLogger(Application.class);
-
-	public static void main(String args[]) {
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Application.class);
-	}
-
-	@Bean
-	public RestTemplate restTemplate(RestTemplateBuilder builder) {
-		return builder.build();
-	}
-
-	@Bean
-	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
-		return args -> {
-			Quote quote = restTemplate.getForObject(
-					"http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
-			log.info(quote.toString());
-		};
 	}
 }
